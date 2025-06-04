0cbde2c60a7b34c3de2d7d54545589557591f1db
Mikelemag <mikelepinto2017@gmail.com>
2025-06-04 11:47:14 -0300

    middlewares, routes

---
+++ src/controllers/produtos.js
@@ -0,0 +1,46 @@
+const produtosModel = require('../models/produtos')
+
+async function getProdutos(req, res) {
+        try {
+
+            
+            const produtos = await produtosModel.getProdutos()
+            
+            return res.send(produtos)
+        } catch (error) {
+            console.error(error)
+            return res.status(500).send('Erro ao cadastrar produto')
+        }
+    }
+
+async function createProduto(req, res) {
+    try {
+        const {nome, catergoria, preco, image_url } = req.body;
+        
+        const produto = await produtosModel.createProduto(req.body)
+
+        return res.status(201).send(produto)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao criar produto!')
+    }
+
+}
+ async function deleteProduto(req, res){
+    const { id } = req.params;
+    try {
+        await produtosModel.deleteProduto(id)
+        return res.status(202).send('Produto deletado com sucesso')
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao deletar produto')
+        
+    }
+ }
+
+    module.exports = {
+        getProdutos,
+        createProduto,
+        deleteProduto
+
+    }
