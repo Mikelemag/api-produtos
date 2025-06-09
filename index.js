140595ff402fb4f826cdfcf2fd149fe92d5f325a
Mikelemag <mikelepinto2017@gmail.com>
2025-06-09 09:24:19 -0300

    TESTE-API

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const port = 6579;
 require('./src/models')
 const produtosRoutes = require('./src/routes/produtos')
 const usersRouter = require('./src/routes/users')
+const authRoutes = require('./src/routes/auth')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(produtosRoutes)
 app.use(usersRouter)
+app.use(authRoutes)
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -2,16 +2,25 @@ const express = require('express')
 const router = express.Router()
 const produtosController = require('../controllers/produtos')
 const produtosMiddleware = require("../middlewares/produtos")
+const authMiddleware = require ('../middlewares/auth')
 
 
-router.get('/produtos', produtosController.getProdutos)
-router.post('/produtos', 
-    produtosMiddleware.validateCreateProduto,
+router.get(
+    '/produtos', 
+    authMiddleware.validateToken,
+    produtosController.getProdutos
+)
+router.post(
+    '/produtos',
+    authMiddleware.validateToken,
+    produtosMiddleware.validateCreateProduto,    
     produtosController.createProduto
 )
-
-router.delete('/produtos/:id',
+router.delete(
+    '/produtos/:id',
+    authMiddleware.validateToken,
     produtosMiddleware.validateDeleteProduto,
     produtosController.deleteProduto
 )
+
 module.exports = router;
